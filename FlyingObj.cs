40a538c057d5bf553c2fa5c2f6051bd780e760fe
PavelD <nagellstev@ya.ru>
2022-12-24 12:15:16 +0400

    Add project files.

---
+++ FlyingObj.cs
@@ -0,0 +1,97 @@
+﻿namespace FlyingObj
+{
+    interface IFlyable
+    {
+        void FlyTo(Coordinate coordinate);
+        double GetFlyTime(Coordinate coordinate);
+    }
+
+    public struct Coordinate
+    {
+        public double X;
+        public double Y;
+        public double Z;
+        public void Print()
+        {
+            Console.WriteLine($"X: {X}; Y: {Y}; Z: {Z}");
+        }
+        public void Input(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+        public Coordinate(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+        public Coordinate()
+        {
+            X = 0;
+            Y = 0;
+            Z = 0;
+        }
+    }
+    public class Bird : IFlyable
+    {
+        public Coordinate initCoord = new Coordinate();
+        public Coordinate finCoord = new Coordinate();
+        public double Speed;
+
+        public void FlyTo(Coordinate coordinate)
+        {
+            initCoord = finCoord;
+            finCoord = coordinate;
+        }
+        public virtual double GetFlyTime(Coordinate coordinate)
+        {
+            Random rnd = new Random();
+            Speed = rnd.Next(20);
+            double time = 0;
+
+            time = Distance(initCoord, finCoord) / Speed;
+
+            return time;
+        }
+        public double Distance(Coordinate init, Coordinate fin)
+        {
+            double d;
+            d = Math.Sqrt(Math.Pow(fin.X - init.X, 2) + Math.Pow(fin.Y - init.Y, 2) + Math.Pow(fin.Z - init.Z, 2));
+            return d;
+        }
+    }
+    public class Airplane : Bird
+    {
+        public override double GetFlyTime(Coordinate coordinate)
+        {
+            double speed = 200;
+            //double acceleration = 10;
+            double time = 0;
+            double d = Distance(initCoord, finCoord);
+            double currDist = 0;
+            while (currDist < d)
+            {
+                currDist += 10;
+                time += 10 / speed;
+                speed += 10;
+            }
+            time += (d - currDist + 10) / speed;
+            Speed = d / time;
+            //time = Math.Sqrt(Math.Pow(initSpeed, 2) + 2 * acceleration * d) - initSpeed;
+            return time;
+        }
+    }
+    public class Drone : Bird
+    {
+        public override double GetFlyTime(Coordinate coordinate)
+        {
+            double speed = 20;
+            double d = Distance(initCoord, finCoord);
+            double time = d / speed + Math.Floor(d / speed / 10);
+            Speed = d / time;
+            return time;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using FlyingObj;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("\tFlying objects");
+        Console.WriteLine("\n");
+        Console.WriteLine("Choose your fighter:");
+        Console.WriteLine("\t1: A bird");
+        Console.WriteLine("\t2: A plane");
+        Console.WriteLine("\t3: A drone");
+        Console.WriteLine("And type number: ");
+
+        Coordinate coord = new Coordinate();
+        TimeSpan time;
+
+        switch (Console.ReadLine())
+        {
+            case "1":
+                Bird bird = new Bird();
+                coord.Input(InputX("X"), InputX("Y"), InputX("Z"));
+                bird.FlyTo(coord);
+                //bird.initCoord.Print();
+                //bird.finCoord.Print();
+                time = TimeSpan.FromHours(bird.GetFlyTime(coord));
+                Console.WriteLine("Bird's Flight Time: " + time.ToString(@"hh\:mm\:ss"));
+                Console.WriteLine("Bird's speed was: " + bird.Speed + "km/h");
+                Console.WriteLine("Covered distance: " + bird.Distance(bird.initCoord, bird.finCoord) + "km");
+                break;
+            case "2":
+                Airplane plane = new Airplane();
+                coord.Input(InputX("X"), InputX("Y"), InputX("Z"));
+                plane.FlyTo(coord);
+                time = TimeSpan.FromHours(plane.GetFlyTime(coord));
+                Console.WriteLine("Plane's Flight Time: " + time.ToString(@"hh\:mm\:ss"));
+                Console.WriteLine("Plane's average speed was: " + plane.Speed + "km/h");
+                Console.WriteLine("Covered distance: " + plane.Distance(plane.initCoord, plane.finCoord) + "km");
+                break;
+            case "3":
+                Drone drone = new Drone();
+                coord.Input(InputX("X"), InputX("Y"), InputX("Z"));
+                drone.FlyTo(coord);
+                time = TimeSpan.FromHours(drone.GetFlyTime(coord));
+                Console.WriteLine("Drone's Flight Time: " + time.ToString(@"hh\:mm\:ss"));
+                Console.WriteLine("Drones's average speed was: " + drone.Speed + "km/h");
+                Console.WriteLine("Covered distance: " + drone.Distance(drone.initCoord, drone.finCoord) + "km");
+                break;
+            default:
+                Console.WriteLine("wdwd");
+                break;
+        }
+    }
+    public static double InputX(string strX)
+    {
+        string XInput;
+        Console.WriteLine("Type " + strX + ", press Enter");
+        XInput = Console.ReadLine();
+        double X = 0;
+        while (!double.TryParse(XInput, out X))
+        {
+            Console.WriteLine("Input decimal number:");
+            XInput = Console.ReadLine();
+        }
+        return X;
+    }
+}
+
+
+

57255a88610ec473f94a6290bb96085427d8a940
PavelD <nagellstev@ya.ru>
2022-12-26 16:27:59 +0400

    added description to the README

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Numerics;
+﻿/// See README.md for description
+
+using System.Numerics;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 using FlyingObjects;
